d39ec4670208e68d28cf37e742a5bc3cfb8bc55b
Almog Noach <almog81@gmail.com>
2023-12-27 14:03:45 +0200

    Updating Google test

--- PageObjects/google/main_page.py
+++ PageObjects/google/main_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from Utilities.Common_ops import *
 from PageObjects.base_page import BasePage
@@ -5,7 +6,8 @@ from PageObjects.base_page import BasePage
 
 class MainPage(BasePage):
 
-    txt_username = (By.ID, "user-name")
+    txt_search = (By.ID, "APjFqb")
+    btn_search = (By.NAME, "btnK")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -13,5 +15,5 @@ class MainPage(BasePage):
     def open_page(self):
         self.driver.get(get_data("googleTest/Url"))
 
-    def search_action(self):
-        print('search done')
+    def search_action(self, word):
+        self.driver.find_element(*self.txt_search).send_keys(word + Keys.ENTER)
--- tests/google_test.py
+++ tests/google_test.py
@@ -5,4 +5,4 @@ from Utilities.Common_ops import *
 def test_google(driver):
     main_page = MainPage(driver)
     main_page.open_page()
-    main_page.search_action()
+    main_page.search_action("pytest")
